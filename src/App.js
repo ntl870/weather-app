2045055f6ed0f575a9afc1b9d644f3d7c9349e23
ntl870 <ntlong870@gmail.com>
2021-02-01 10:04:18 +0700

    finished fetching weather

--- src/App.js
+++ src/App.js
@@ -1,52 +1,59 @@
-import React, { useEffect, useState } from "react";
-// import SearchBar from "./Components/SearchBar/SearchBar";
-import axios from "axios";
+import React, { useState } from "react";
+
 const api = {
-  key: "61dec65e599e81c7da0ba7b73d1a6349",
+  key: "b1451a60f85568577babe763a87da932",
   base: "https://api.openweathermap.org/data/2.5/",
 };
 
 const App = () => {
-  const [Input, setInput] = useState("");
-  const [City, setCity] = useState("");
+  const [input, setInput] = useState("");
+  const [weather, setWeather] = useState({});
 
-  const Search = () => {
-    fetch(`${api.base}weather?q=London&appid=${api.key}`)
-      .then((res) => {
-        res.json();
-        console.log(res);
-      })
-      .then((result) => {
-        console.log(result);
-      });
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${input}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setInput("");
+          // console.log(result);
+        });
+    }
   };
-
-  const fetchData = () => {
-    axios({
-      method: "GET",
-      url: `${api.base}weather?id=2172797&appid=${api.key}`,
-    })
-      .then((response) => {
-        response.json();
-      })
-      .then((res) => {
-        res.json();
-        console.log(res);
-      });
-  };
-
- 
-
-  Search();
-
-  return (
-    <div className="App">
-      <form>
-        <input type="text" />
-        <button onClick={() => setCity("New York")}>Search</button>
-      </form>
-    </div>
-  );
+  if (typeof weather.main == "undefined") {
+    console.log(weather)
+    return (
+      <div className="App">
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+            onKeyPress={search}
+          />
+        </div>
+      </div>
+    );
+  } else {
+    console.log(weather);
+    return (
+      <div className="App">
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+            onKeyPress={search}
+          />
+          <h1>{weather.main.temp}</h1>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default App;
